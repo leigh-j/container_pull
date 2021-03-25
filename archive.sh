ad1844cb302380f0dfe58c36dfb40cef97703ad9
fedora Cloud User <fedora@ip-10-16-77-210.ap-southeast-2.compute.internal>
2021-03-25 01:06:39 +0000

    a few updates, last 4.6 mirror

--- archive.sh
+++ archive.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-ARCH_DIRS="helm/ aarnet/ registry/ wget/ ocp-release/ olm/ bin/"
+PREF=ek8s
+#ARCH_DIRS="registry/ helm/ aarnet/ wget/ ocp-release/ olm/ bin/"
+ARCH_DIRS="registry/ "
 pushd /var/lib/import
-tar -cvf - $ARCH_DIRS | split --bytes=10240m --suffix-length=3 --numeric-suffix - ../export/enterprise-k8s.tar.
+rm -Rf ../export/*
+tar -cf - $ARCH_DIRS | split --bytes=10240m --suffix-length=3 --numeric-suffix - ../export/$PREF.tar.
+pushd /var/lib/export
+sha256sum $PREF.tar* > sha256.list
 
