fba7730529aba0ac53527622c5183b852e814ba6
fedora Cloud User <fedora@ip-10-16-77-210.ap-southeast-2.compute.internal>
2021-04-14 02:46:38 +0000

    updated helm and ocp mirroring

--- archive.sh
+++ archive.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
 PREF=ek8s
-#ARCH_DIRS="registry/ helm/ aarnet/ wget/ ocp-release/ olm/ bin/"
-ARCH_DIRS="registry/ "
-pushd /var/lib/import
-rm -Rf ../export/*
-tar -cf - $ARCH_DIRS | split --bytes=10240m --suffix-length=3 --numeric-suffix - ../export/$PREF.tar.
-pushd /var/lib/export
-sha256sum $PREF.tar* > sha256.list
+ARCH_DIRS=(helm aarnet wget registry)
+#ARCH_DIRS="registry/ "
+for source in ${ARCH_DIRS[@]}; do
+  pushd /var/lib/import
+  rm -Rf ../export/$PREF-$source.tar*
+  tar -cf - $source | split --bytes=10240m --suffix-length=3 --numeric-suffix - ../export/$PREF-$source.tar.
+  pushd /var/lib/export
+  for part in $(find ./ -name "ek8s-$source.tar.*" | grep -v sha25); do
+    sha256sum $part > $part.sha256sum
+  done
+done
+#sha256sum $PREF.tar* > sha256.list
 
